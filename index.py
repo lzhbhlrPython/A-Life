2eab4936b44383ac0b82f54cbb3496acb168e204
Lzhbhlrpython <Liubaolin20070125@outlook.com>
2022-03-21 10:40:59 +0800

    inital

--- index.py
+++ index.py
@@ -6,27 +6,10 @@ def random_name(length):
 def random_dna(length):
     return ''.join(random.choices(string.ascii_letters+string.digits,k=length))
 
-def main():
-    l1=Life(random_name(5),random_dna(DNA_LENGTH))
-    l2=Life(random_name(5),random_dna(DNA_LENGTH))
-    l3=l1+l2
-    l4=l1+l2
-    l5=l3+l4
-
-    print(l1)
-    print(l2)
-    print(l3)
-    print(l4)
-    print(l5)
-    print(l3==l4)
-    like_v=0
-    for i,j,k in zip(l5.dna,l1.dna,l2.dna):
+def compare_dna(lifep_1,lifep_2,life_c):
+    simlar_v=0
+    for i,j,k in zip(lifep_1.dna,lifep_2.dna,life_c.dna):
         if i==j or i==k:
-            like_v+=1
-    print(like_v)
+            simlar_v+=1
+    return simlar_v
 
-if __name__=='__main__':
-    while True:
-        main()
-        time.sleep(1)
-        print("\n")
